ac0038ef6d1246652d3f0e6efe16de73ea0f5af2
hahn-jehee <69448158+hahn-jehee@users.noreply.github.com>
2020-08-11 21:00:48 +0900

    0811 day1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button, Progress} from 'semantic-ui-react'
 function App() {
   return (
+    <div>
     <p> hahn-jehee </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicationg/>
+    </div>
   );
 }
 
