8fe1b503dadf607987d6692ff3b5efe2ee55be9e
hahn-jehee <69448158+hahn-jehee@users.noreply.github.com>
2020-08-14 11:12:45 +0900

    0813 최종

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 import React from 'react';
-import {Image, Comment, Form, Button, Header} from 'semantic-ui-react'
+import {Image} from 'semantic-ui-react'
+
+import Comments from './comment.js'
 
 import ato from "./ato.jpg"
-import atoface from "./atoface.jpg"
-
-class Comments extends React.Component{
-  render(){
-    return(  <Comment.Group>
-        <Header as='h3' dividing>
-          Comments
-        </Header>
-
-        <Comment>
-          <Comment.Avatar src={atoface} />
-          <Comment.Content>
-            <Comment.Author as='a'>Matt</Comment.Author>
-            <Comment.Metadata>
-              <div>Today at 5:42PM</div>
-            </Comment.Metadata>
-            <Comment.Text>How artistic!</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-
-        <Form reply>
-          <Form.TextArea />
-          <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-        </Form>
-      </Comment.Group>)
-  }
-}
 
 function App() {
   return (
