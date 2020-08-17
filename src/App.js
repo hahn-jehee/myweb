968c9594ce6befd6f52e6c77d3ef5b2e257c949a
hahn-jehee <69448158+hahn-jehee@users.noreply.github.com>
2020-08-18 02:24:46 +0900

    0816 (3차시)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import {Image} from 'semantic-ui-react'
+import React from "react";
+import {
+  Button,
+  Image,
+  Divider,
+  Header,
+  Icon,
+  Grid,
+  Label,
+} from "semantic-ui-react";
 
-import Comments from './comment.js'
+import Comments from "./comment.js";
+import Buttons from "./buttons.js";
 
-import ato from "./ato.jpg"
+import ato from "./ato.jpg";
 
 function App() {
   return (
-    <div>
-    <Image src = {ato} style = {{"width": 400, "height":400}} centered />
-    <Comments />
+    <div style = {{backgroundColor : "white"}}>
+      <Grid centered>
+        <Grid.Row>
+          <Image src={ato} style={{ width: 600, height: 600 }} centered />
+        </Grid.Row>
+
+        <Grid.Row>
+          <Buttons />
+        </Grid.Row>
+      </Grid>
+      <br />
+
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="comments" style = {{color : "black"}}/>
+          Comments
+        </Header>
+      </Divider>
+
+      <Comments />
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="share alternate" />
+          Contact
+        </Header>
+      </Divider>
+      <br />
+      <div>
+        <Grid centered>
+          <Button circular color="facebook" size="small" icon="facebook" />
+          <Button circular color="twitter" size="small" icon="twitter" />
+          <Button
+            circular
+            color="instagram"
+            size="small"
+            icon="instagram"
+            onClick={() =>
+              window.open("https://www.instagram.com/j_e_heeeeee/")
+            }
+          />
+          <Button circular color="youtube" size="small" icon="youtube" />
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/comment.js
+++ src/comment.js
@@ -1,35 +1,71 @@
-import React from 'react';
-import {Comment, Form, Button, Header} from 'semantic-ui-react'
+import React from "react";
+import { Comment, Form, Button, Header, Icon } from "semantic-ui-react";
 
-import atoface from "./atoface.jpg"
+import atoface from "./atoface.jpg";
 
-class Comments extends React.Component{
-  render(){
-    return(  <Comment.Group>
-        <Header as='h3' dividing>
+function SingleComment(detail) {
+  return (
+    <Comment>
+      <Comment.Content>
+        <Comment.Avatar src={atoface} />
+        <Comment.Author as="a">방문자</Comment.Author>
+        <Comment.Metadata>
+          <div>Today</div>
+        </Comment.Metadata>
+        <Comment.Text>{detail.content}</Comment.Text>
+      </Comment.Content>
+    </Comment>
+  );
+}
+
+class Comments extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputcontent: "",
+      commentsList: [],
+    };
+  }
+
+  render() {
+    console.log(this.state.commentsList);
+    return (
+      <Comment.Group style={{ marginLeft: "530px" }}>
+        <Header as="h3" dividing>
           Comments
         </Header>
 
-        <Comment>
-          <Comment.Avatar src={atoface} />
-          <Comment.Content>
-            <Comment.Author as='a'>Matt</Comment.Author>
-            <Comment.Metadata>
-              <div>Today at 5:42PM</div>
-            </Comment.Metadata>
-            <Comment.Text>How artistic!</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
+        {this.state.commentsList.map((comments) => (
+          <SingleComment content={comments} />
+        ))}
 
         <Form reply>
-          <Form.TextArea />
-          <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+          <Form.TextArea
+            value={this.state.inputContent}
+            placeholder="Thank you for the comment"
+            onChange={(e) => this.setState({ inputContent: e.target.value })}
+          />
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+            onClick={() =>
+              this.setState((prevState) => {
+                return {
+                  commentsList: [
+                    ...prevState.commentsList,
+                    this.state.inputContent,
+                  ],
+                  inputContent: "",
+                };
+              })
+            }
+          />
         </Form>
-      </Comment.Group>)
+      </Comment.Group>
+    );
   }
 }
 
-export default Comments
+export default Comments;
